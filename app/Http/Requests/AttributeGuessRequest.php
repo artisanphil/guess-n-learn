1277c06db9186a3c8d97d8e2e9d9c499118b07d0
Philip <philip@learnclick.com>
2021-10-30 19:09:00 +0100

    removed checking for sentence in request validation

--- app/Http/Requests/AttributeGuessRequest.php
+++ app/Http/Requests/AttributeGuessRequest.php
@@ -3,25 +3,17 @@
 namespace App\Http\Requests;
 
 use Illuminate\Validation\Rule;
-use App\Repositories\SentenceRepository;
+use App\Repositories\ObjectRepository;
 use Illuminate\Foundation\Http\FormRequest;
 
 class AttributeGuessRequest extends FormRequest
 {
     public function rules()
     {
-        $sentenceRepository = new SentenceRepository();
-        $correctSentence = $sentenceRepository->getSentenceByAttribute($this->choice, false);
-
         return [
             'choice' => [
                 'required',
                 'string',
-            ],
-            'sentence' => [
-                'required',
-                'string',
-                Rule::in([$correctSentence])
             ]
         ];
     }
